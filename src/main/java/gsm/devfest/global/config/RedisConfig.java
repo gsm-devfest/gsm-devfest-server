5ebd56679583b8768c1a7b4273deee70c10e7f8f
yoontaebin <gusckd000@gmail.com>
2024-05-21 16:10:28 +0900

    create redis config

---
+++ src/main/java/gsm/devfest/global/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package gsm.devfest.global.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.data.redis")
+public class RedisConfig {
+
+    private String host;
+    private int port;
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + host + ":" + port)
+                .setDnsMonitoringInterval(-1);
+        return Redisson.create(config);
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(RedissonClient redissonClient) {
+        return new RedissonConnectionFactory(redissonClient);
+    }
+
+}
