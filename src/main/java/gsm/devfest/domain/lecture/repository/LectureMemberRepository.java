431b0839d4d1df5e34e6a0a4134af18fcd7bfa11
yoontaebin <gusckd000@gmail.com>
2024-05-30 12:26:40 +0900

    create LectureMemberRepository

---
+++ src/main/java/gsm/devfest/domain/lecture/repository/LectureMemberRepository.java
@@ -0,0 +1,10 @@
+package gsm.devfest.domain.lecture.repository;
+
+import gsm.devfest.domain.lecture.entity.LectureMember;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
+
+public interface LectureMemberRepository extends R2dbcRepository<LectureMember, Long> {
+
+    Mono<Boolean> existsByLectureIdAndMemberId(Long lectureId, Long memberId);
+}
