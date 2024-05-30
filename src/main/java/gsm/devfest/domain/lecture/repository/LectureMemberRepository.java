5f82cb7cc5cdf6b4d8e1a6d517039473765fa5c9
yoontaebin <gusckd000@gmail.com>
2024-05-30 14:57:46 +0900

    update LectureMemberRepository

--- src/main/java/gsm/devfest/domain/lecture/repository/LectureMemberRepository.java
+++ src/main/java/gsm/devfest/domain/lecture/repository/LectureMemberRepository.java
@@ -4,7 +4,10 @@ import gsm.devfest.domain.lecture.entity.LectureMember;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDate;
+
 public interface LectureMemberRepository extends R2dbcRepository<LectureMember, Long> {
 
     Mono<Boolean> existsByLectureIdAndMemberId(Long lectureId, Long memberId);
+    Mono<Boolean> existsByMemberIdAndLectureDate(Long memberId, LocalDate lectureDate);
 }
