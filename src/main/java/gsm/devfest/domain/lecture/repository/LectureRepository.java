bea5fdcda88f9aa9e032e57797ee6d7ddd9ad3ff
yoontaebin <gusckd000@gmail.com>
2024-06-03 13:35:25 +0900

    add findAllByLectureDate method

--- src/main/java/gsm/devfest/domain/lecture/repository/LectureRepository.java
+++ src/main/java/gsm/devfest/domain/lecture/repository/LectureRepository.java
@@ -2,6 +2,11 @@ package gsm.devfest.domain.lecture.repository;
 
 import gsm.devfest.domain.lecture.entity.Lecture;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDate;
 
 public interface LectureRepository extends R2dbcRepository<Lecture, Long> {
+
+    Flux<Lecture> findAllByLectureDate(LocalDate lectureDate);
 }
