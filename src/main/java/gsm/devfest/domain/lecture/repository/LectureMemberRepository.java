5ab163b07147654dbf9f2782edd86da716f9ecdb
yoontaebin <gusckd000@gmail.com>
2024-06-03 13:34:52 +0900

    delete existsByMemberIdAndLectureDate method

--- src/main/java/gsm/devfest/domain/lecture/repository/LectureMemberRepository.java
+++ src/main/java/gsm/devfest/domain/lecture/repository/LectureMemberRepository.java
@@ -4,10 +4,9 @@ import gsm.devfest.domain.lecture.entity.LectureMember;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Mono;
 
-import java.time.LocalDate;
 
 public interface LectureMemberRepository extends R2dbcRepository<LectureMember, Long> {
-
     Mono<Boolean> existsByLectureIdAndMemberId(Long lectureId, Long memberId);
-    Mono<Boolean> existsByMemberIdAndLectureDate(Long memberId, LocalDate lectureDate);
+
 }
+
