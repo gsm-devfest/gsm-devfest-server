30c58fbbe48229079635e2bf5f005e3c93aa3e89
yoontaebin <gusckd000@gmail.com>
2024-06-03 18:12:41 +0900

    update add lecture search, delete

--- src/main/java/gsm/devfest/domain/lecture/controller/LectureController.java
+++ src/main/java/gsm/devfest/domain/lecture/controller/LectureController.java
@@ -1,14 +1,13 @@
 package gsm.devfest.domain.lecture.controller;
 
 import gsm.devfest.domain.lecture.data.CreateLectureRequest;
+import gsm.devfest.domain.lecture.data.LectureResponse;
 import gsm.devfest.domain.lecture.data.RegisterLectureRequest;
 import gsm.devfest.domain.lecture.service.LectureService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
@@ -30,5 +29,22 @@ public class LectureController {
     public Mono<ResponseEntity<Long>> registerLecture(@RequestBody RegisterLectureRequest request) {
         return lectureService.registerLecture(request)
                 .map(ResponseEntity::ok);
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<LectureResponse>> getLectureById(@PathVariable Long id) {
+        return lectureService.getLectureById(id)
+                .map(ResponseEntity::ok);
+    }
+
+    @GetMapping
+    public ResponseEntity<Flux<LectureResponse>> getLecture() {
+        return ResponseEntity.ok(lectureService.getLecture());
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Void>> deleteLecture(@PathVariable Long id) {
+        return lectureService.deleteLecture(id)
+                .then(Mono.just(ResponseEntity.noContent().build()));
     }
 }
